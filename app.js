87ab1a9152d0cd6343061ad9e3567d79c326d7ad
KantoCanto <bruno.canto.neves@gmail.com>
2022-07-01 20:14:32 +0100

    app ready, html and css missing

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const fetchPokemon = () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/1";
-    fetch(url)
-        .then( (res) => {
-            return res.json();
-        })
-        .then( (data) => {
-            console.log(data)
-            const pokemon = {};
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            console.log(pokemon);
-        })
+
+    //array of promises
+    const promises = [];
+
+    //itterate from 1 to 150 and call the api in order to fetch each of the pokemons data
+    for (let i = 1; i <= 150; i++){
+
+        const url = 'https://pokeapi.co/api/v2/pokemon/' + i;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then( results => {
+
+        //the map function will itterate through an array of items and create
+        //a new array by converting each item in some way
+        const pokemon = results.map( data => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types
+            .map((type) => type.type.name)
+            .join(', ')
+        }))
+        displayPokemon(pokemon);
+    })
+}
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon)
 }
 
 fetchPokemon();
