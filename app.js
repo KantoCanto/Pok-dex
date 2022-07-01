1681a6c527638e8a8be70d28694cd0dc161f61e6
KantoCanto <bruno.canto.neves@gmail.com>
2022-07-01 18:07:33 +0100

    star

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-console.log("Meow")
-
 const fetchPokemon = () => {
-    
+    const url = "https://pokeapi.co/api/v2/pokemon/1";
+    fetch(url)
+        .then( (res) => {
+            return res.json();
+        })
+        .then( (data) => {
+            console.log(data)
+            const pokemon = {};
+            pokemon['name'] = data.name;
+            pokemon['id'] = data.id;
+            console.log(pokemon);
+        })
 }
+
+fetchPokemon();
